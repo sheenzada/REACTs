41011286f88570a7c744b1c54634e41579d585ba
Inamullah <sheenzada7@gmail.com>
2026-02-20 02:47:07 -0800

    react

---
+++ src/Component/Question.jsx
@@ -0,0 +1,10 @@
+export default function Question (){
+    return(
+        <section className="min-h-screen bg-slate-300">
+            <div className="w-full h-24 bg-gray-400 mt-10">
+
+            </div>
+
+        </section>
+    )
+}
